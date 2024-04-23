14819b6be486b09867e2478ffe2e6e378bb8a5bb
Stef Verlinde <stefverlinde@hotmail.com>
2024-04-23 22:50:38 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-red-500">Hallooo</h1>
+      <Button variant="destructive">Hello</Button>
+    </div>
+  )
+}
+
+export default App
